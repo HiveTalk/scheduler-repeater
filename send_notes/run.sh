7f72c6f343d8673ac1fcf5bd85e5c977905aaf01
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2025-03-23 22:38:32 -0700

    compile on local arch

--- send_notes/run.sh
+++ send_notes/run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 cd /root/scheduler/send_notes
+
+# Make sure Go is in the PATH
+export PATH=$PATH:/usr/local/go/bin
+
+# Check if the Go binary exists, if not, compile it
+if [ ! -f "./send_notes" ] || [ "$(file ./send_notes | grep -i 'executable')" == "" ]; then
+    echo "Compiling send_notes binary..."
+    go build -o send_notes
+fi
+
+# Run the binary
 ./send_notes
